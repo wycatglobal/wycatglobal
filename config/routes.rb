1daf5043103e2114ff59f337382dc729128c0782
wycat <reachwycat@gmail.com>
2018-08-29 15:41:14 +0530

    Membership Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'upload' => 'images#new', :as => "upload"
+  get 'membership' => 'memberships#new'
   resources :albums, :images
   root 'images#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
